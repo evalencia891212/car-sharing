f9b4b36a663c35b4558701d434da5ab886ac40fe
evalencia891212 <emmanuel.valencia@42-q.com>
2024-05-28 14:42:38 -0600

    Test API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 
 export const environment = {
   production: true,
-  apiKey: 'prodKey',
+  mapBoxToken: 'pk.eyJ1IjoiZW1tYW51ZWw4OSIsImEiOiJjbHUyMmQ2emYwZ2lzMmp0Z2d1ZmZlMHhuIn0.94uy3X09yt6AQrArEw4bvg',
   apiHost: "https://eco-car-api-882f9a366724.herokuapp.com"
 };
