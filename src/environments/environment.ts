42e69d809fae68826254b58312642a617d53ace4
evalencia891212 <emmanuel.valencia@42-q.com>
2024-05-28 14:39:42 -0600

    Test API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,11 @@
-export const environment = {
-    production: false,
-    mapBoxToken: 'pk.eyJ1IjoiZW1tYW51ZWw4OSIsImEiOiJjbHUyMmQ2emYwZ2lzMmp0Z2d1ZmZlMHhuIn0.94uy3X09yt6AQrArEw4bvg',
-    apiHost: "http://localhost:3000"
-  };
+// export const environment = {
+//     production: false,
+//     mapBoxToken: 'pk.eyJ1IjoiZW1tYW51ZWw4OSIsImEiOiJjbHUyMmQ2emYwZ2lzMmp0Z2d1ZmZlMHhuIn0.94uy3X09yt6AQrArEw4bvg',
+//     apiHost: "http://localhost:3000"
+//   };
 
+export const environment = {
+  production: true,
+  apiKey: 'prodKey',
+  apiHost: "https://eco-car-api-882f9a366724.herokuapp.com"
+};
